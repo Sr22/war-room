0a3442ea069c86010a51c86b298e0d2675eafcc3
Jake Springer` <Jake.Springer@cdk.com>
2016-06-27 10:44:24 -0700

    Widget scripts are executed in the global scope.

--- app/components/add-widget.js
+++ app/components/add-widget.js
@@ -1,25 +1,22 @@
 
 
 var loadScriptCallback = function(src, callback) {
-  var script = document.createElement('script');
-  var loaded = false;
-  script.setAttribute('src', src);
-  if (callback) {
-    script.onreadystatechange = script.onload = function() {
-      if (!loaded) {
+  return (function (src, callback) {
+    $.ajax({
+      url : src,
+      success : function (data) {
+        eval.call(window, data);
         callback();
       }
-      loaded = true;
-    };
-  }
-  document.getElementsByTagName('head')[0].appendChild(script);
+    });
+  })(src, callback);
 }
 
 angular.module("warRoom")
     .factory('addWidget', ['$compile', function($compile) {
       var service = {};
       service.addWidget = function(widget, scope, callback) {
-        return function (widget, scope, callback) {
+        return (function (widget, scope, callback) {
           if (widget instanceof Array) {
             var body = '<ww-widget data-row=1 data-col=1 data-sizex=1 data-sizey=2><include-executable-file src="' + widget[0] + '"></include-executable-file></ww-widget><span class="gs-remove-handle gs-remove-handle-all"></span>';
             widget.filter(function (elem) { return elem.endsWith(".css"); })
@@ -30,8 +27,8 @@ angular.module("warRoom")
                   link.setAttribute('href', elem);
                   document.getElementsByTagName('head')[0].appendChild(link);
                 });
-            widget.filter(function (elem) { return elem.endsWith(".js"); })
-                .forEach(function (elem) {
+            var jsFiles = widget.filter(function (elem) { return elem.endsWith(".js"); });
+            jsFiles.forEach(function (elem) {
                   loadScriptCallback(elem, function() {
                     var el = angular.element(body);
                     var compiled = $compile(el);
@@ -40,6 +37,13 @@ angular.module("warRoom")
                     callback();
                   });
                 });
+            if (jsFiles.length == 0) {
+              var el = angular.element(body);
+              var compiled = $compile(el);
+              $('.ww-widgets-ul').append(el);
+              compiled(scope);
+              callback();
+            }
           } else {
             var body = '<ww-widget data-row=1 data-col=1 data-sizex=1 data-sizey=2><include-executable-file src="' + widget + '"></include-executable-file></ww-widget><span class="gs-remove-handle gs-remove-handle-all"></span>';
             var el = angular.element(body);
@@ -48,7 +52,7 @@ angular.module("warRoom")
             compiled(scope);
             callback();
           }
-        }(widget, scope, callback);
+        })(widget, scope, callback);
       }
       
       return service;
