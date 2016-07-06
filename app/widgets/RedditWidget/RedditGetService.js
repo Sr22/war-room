4de8b3b476827b7c62c88a4347022a85b52214c0
Davis <jack.davis@cdk.com>
2016-07-06 11:26:45 -0700

    Base Reddit Widget

--- app/widgets/RedditWidget/RedditGetService.js
+++ app/widgets/RedditWidget/RedditGetService.js
@@ -2,36 +2,36 @@
  * Created by davisjac on 6/29/2016.
  */
 angular.module("warRoom")
-    .value("HOST", "http://www.reddit.com/r/")
-    .service("getPostInfo", ["search", "getPostInfo"]);
-
-function getTitles(redditSearch) {
+    .service("redditService", [ '$http', '$q', function($http, $q) {
     return {
-        retrievePostInformation: function() {
-            var posts = esriRequest({
-                url: HOST + redditSearch + "/top/.json?limit=3",
-                handleAs: "json"
-            });
-            posts.then(function() {
-                var pos = [{
-                    title: '',
-                    posturl: ''
-                }];
-                var i = 0;
-                while (i != 3) {
-                    if (pos.title = '') {
-                        pos.title = data.data.children[i].title;
-                        pos.posturl = data.data.children[i].url;
-                    }
-                    else {
-                        var temp = {title: data.data.children[i].title, posturl: data.data.children[i].url};
-                        pos = pos + temp;
-                    }
-                }
-                return pos;
-            }, function() {
-                return null;
+        retrievePostInformation: function(redditSearch) {
+            var defer = $q.defer();
+            return $http.get("http://www.reddit.com/r/" + redditSearch + "/top/.json?limit=3").then(function onSuccess(response) {
+                console.log('SUCCESS');
+                var tempTopic = {
+                    name: redditSearch,
+                    posts: [{
+                            title: response.data.data.children[0].data.title,
+                            postUrl: response.data.data.children[0].data.url
+                        },
+                        {
+                            title: response.data.data.children[1].data.title,
+                            postUrl: response.data.data.children[1].data.url
+                        },
+                        {
+                            title: response.data.data.children[2].data.title,
+                            postUrl: response.data.data.children[2].data.url
+                        }
+                    ]
+                };
+                defer.resolve(tempTopic);
+                console.log(defer)
+                return defer.promise;
+            }, function onError(response) {
+                console.log('ERROR');
+                defer.reject(response);
+                return defer.promise;
             })
         }
     }
-}
+}])
--- app/widgets/RedditWidget/redditController.js
+++ app/widgets/RedditWidget/redditController.js
@@ -2,22 +2,26 @@
  * Created by davisjac on 6/29/2016.
  */
 angular.module("warRoom")
-    .controller('rController', ['$scope', 'RedditGetService', function($scope, RedditGetService) {
-        $scope.search = [{ title: ''}];
-        $scope.posts = [{
-            title: '',
-            postURL: ''
-        }];
-        $scope.submit = function(rSearch) {
-            $scope.search = $scope.search + rSearch;
-            $scope.posts = $scope.posts + RedditGetService.retrievePostInfo(rSearch);
+    .controller('rController', ['$scope', 'redditService', function($scope, RedditService) {
+        $scope.search;
+        $scope.topics = [];
+        $scope.submit = function() {
+            RedditService.retrievePostInformation($scope.search).then(function(data) {
+                $scope.$evalAsync(function() {
+                    $scope.topics.push(data);
+                });
+            }).catch(function(error) {
+                console.log('caught it -', error)
+            });
+            $scope.search = '';
         };
     }]);
 
 angular.module("warRoom")
     .directive("redditWidget", function () {
         var directive = {
-            templateUrl: "widgets/redditwidget/redditWidgetDesign.html"
+            templateUrl: "widgets/redditwidget/redditWidgetDesign.html",
+            controller: "rController"
         };
         return directive;
     });
