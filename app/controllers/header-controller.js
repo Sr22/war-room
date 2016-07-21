d9b9b55a2cf4f85e3c0f7f57ae243e87444d2eea
DeYoung <deyoungm@ds.ad.adp.com>
2016-07-21 14:16:25 -0700

    better add and remove

--- app/controllers/header-controller.js
+++ app/controllers/header-controller.js
@@ -1,46 +1,30 @@
 angular.module('warRoom')
-    .controller('headerController', ['$scope', 'widgetService', function ($scope, widgetService) {
-      $scope.saveWidgets = function() {
-        widgetService.saveWidgets();
-      };
-      
-      $scope.loadWidgets = function() {
-        widgetService.removeAllWidgets();
-        widgetService.loadWidgets();
-        widgetService.commit();
-      }
-      
-      $scope.removeWidget = function(widget) {
-        widgetService.removeWidget(widget);
-      }
-      
-      $scope.gridstackEnabled = false;
-      $scope.toggleEnable = function() {
-        $scope.gridstackEnabled = !$scope.gridstackEnabled;
-        widgetService.setWidgetsEnabled($scope.gridstackEnabled);
-      }
-        
-      $scope.addWeather = function() {
-          widgetService.addWidget('WeatherWidget', 0, 0, 2, 4, true);
-      };
 
-      $scope.addTime = function() {
-          widgetService.addWidget('TimeWidget', 0, 0, 2, 4, true);
-      };
+.controller('headerController', ['$scope','widgetList', 'widgetService', function ($scope,widgetList, widgetService) {
+  $scope.saveWidgets = function() {
+    widgetService.saveWidgets();
+  };
 
-      $scope.addCalendar = function() {
-          widgetService.addWidget('CalendarWidget', 0, 0, 2, 4, true);
-      };
+  $scope.loadWidgets = function() {
+    widgetService.removeAllWidgets();
+    widgetService.loadWidgets();
+    widgetService.commit();
+  };
 
-      $scope.addReddit = function() {
-          widgetService.addWidget('RedditWidget', 0, 0, 2, 4, true);
-      };
+   $scope.gridstackEnabled = false;
+  $scope.toggleEnable = function() {
+    $scope.gridstackEnabled = !$scope.gridstackEnabled;
+    widgetService.setWidgetsEnabled($scope.gridstackEnabled);
+  };
 
-      $scope.addNotify = function() {
-          widgetService.addWidget('NotificationsWidget', 0, 0, 2, 4, true);
-      };
+ $scope.addWidget = function(widget, callback) {
+   for (var i = 0; i < widgetList.length; i++) {
+     if (widgetList[i].name === widget) {
+       widgetService.addWidget({content: widgetList[i]}, callback);
+       widgetService.commit();
+     }
+   }
+ };
 
-      $scope.addStash = function() {
-          widgetService.addWidget('StashWidget', 0, 0, 2, 4, true);
-      };
-    }]);
+  $scope.widgetList = widgetList;
+}]);
