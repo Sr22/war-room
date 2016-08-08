8117dc5990f905f42702fcad939b1a375cf7fd0c
Han <hank@ds.ad.adp.com>
2016-08-08 11:04:46 -0700

    Fixing conflicts

--- app/widgets/weather-widget/weather-widget-directive.js
+++
@@ -1,9 +0,0 @@
-angular.module('warRoom')
-    .directive('weatherWidget', function weatherWidget() {
-        return {
-            templateUrl: 'widgets/weather-widget/weather-widget.html',
-            controller: 'weatherWidgetController',
-            bindToController: true
-        }
-    })
-		
