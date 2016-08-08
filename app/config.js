cfc183b81ad2512dd7946b0ddb50be517eab2d34
Han <hank@ds.ad.adp.com>
2016-08-08 11:18:38 -0700

    Fixing

--- app/config.js
+++ app/config.js
@@ -58,7 +58,7 @@ angular
       dependencies: [
         'widgets/weather-widget/weather-api-service.js',
         'widgets/weather-widget/weather-widget-controller.js',
-        'widgets/weather-widget/weather-widget-directive.js',
+        //'widgets/weather-widget/weather-widget-directive.js',
         'widgets/weather-widget/weather-widget.html',
         'widgets/weather-widget/weather-widget.css'
       ],
@@ -66,20 +66,21 @@ angular
       initialize: 'timeWidgetService.initialize',
       serialize:'timeWidgetService.serialize'
     },
-    {
-        name: 'NotesWidget',
-        displayName: 'Notes',
-        dependencies: [
-            'bower_components/showdown/dist/showdown.js',
-            'widgets/notes-widget/notes-widget-service.js',
-            'widgets/notes-widget/notes-widget-controller.js',
-            'widgets/notes-widget/notes-widget.js',
-            'widgets/notes-widget/notes-widget.css'
-        ],
-        directive: 'notesWidget',
-        initialize: 'notesWidgetService.initialize',
-        serialize: 'notesWidgetService.serialize'
-    } {
+      {
+          name: 'NotesWidget',
+          displayName: 'Notes',
+          dependencies: [
+              'bower_components/showdown/dist/showdown.js',
+              'widgets/notes-widget/notes-widget-service.js',
+              'widgets/notes-widget/notes-widget-controller.js',
+              'widgets/notes-widget/notes-widget.js',
+              'widgets/notes-widget/notes-widget.css'
+          ],
+          directive: 'notesWidget',
+          initialize: 'notesWidgetService.initialize',
+          serialize: 'notesWidgetService.serialize'
+      },
+      {
           name: 'NewsWidget' ,
           displayName: 'News',
           dependencies: [
